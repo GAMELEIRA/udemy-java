c0e4b8febca439f0d30f35e0e9310a58cf8778a8
GAMELEIRA <gameleira270499@gmail.com>
2020-01-11 14:32:57 -0300

    Criacao do primeiro projeto JAVA chamado UdemyJavaPratica01, que tem como finalidade exibir uma mensagem de boas vindas na tela.

---
+++ UdemyJavaPratica01/src/application/Main.java
@@ -0,0 +1,18 @@
+package application;
+
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:11/01/2020
+ * HORA:14:10
+ * FINALIDADE DO PROGRAMA: EXIBIR UMA MENSAGEM NA TELA.
+ */
+
+public class Main {
+
+	//METODO QUE EXECUTA A APLICACAO JAVA
+	public static void main(String[] args) {
+
+		System.out.println("Olá, Mundo!");
+		
+	}
+}
