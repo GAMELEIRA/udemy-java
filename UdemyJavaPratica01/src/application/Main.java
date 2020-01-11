53fbc1af632c4548421e07b84080c188dcb814e7
GAMELEIRA <gameleira270499@gmail.com>
2020-01-11 17:24:32 -0300

    Acrescimo de valores para exibição no console no projeto UdemyJavaPratica01

--- UdemyJavaPratica01/src/application/Main.java
+++ UdemyJavaPratica01/src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Locale;
+
 /**
  * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
  * DATA:11/01/2020
@@ -11,8 +13,54 @@ public class Main {
 
 	//METODO QUE EXECUTA A APLICACAO JAVA
 	public static void main(String[] args) {
-
-		System.out.println("Olá, Mundo!");
 		
+		//DECLARACAO DE VARIAVEIS E CONFIGURACOES ADICIONAIS
+		//==========================================
+	
+		int y;
+		
+		double x;
+		
+		String nome;
+		
+		int idade;
+		
+		double renda;
+		
+		Locale.setDefault(Locale.US); //DEFINE CONFIGURACOES NACIONAIS SOBRE PADRONIZACOES 
+		
+		//==========================================
+		
+		//ATRIBUICAO
+		//===================================
+		y = 32;
+		
+		x = 10.35784;
+		
+		nome = "Maria";
+		
+		idade = 31;
+		
+		renda  = 4000.0;
+		
+		//===================================
+		
+		System.out.printf("%.2f", x); 
+		
+		//REMOVA O SOUT, COMPILE E EXECUTE. A LINHA DE CIMA NAO SERA EXIBIDA.
+		System.out.println();
+		
+		System.out.println(y +"\n"+ x);
+		
+		System.out.print("Olá, Mundo!");
+		
+		System.out.println(" Bom dia!");
+		
+		System.out.println("Resultado = " + x + " metros.");
+		
+		System.out.printf("Resultado = %.2f metros. \n", x);
+			
+		System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
+	
 	}
 }
