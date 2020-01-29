06bb0fe4cb6a54f7273854f780829a99a76a0c95
GAMELEIRA <gameleira270499@gmail.com>
2020-01-29 19:52:55 -0300

    Projeto UdemyEstruturaCondicionalExercicioProposto04 criado com o objetivo de indicar a duracao de um jogo

---
+++ UdemyJavaEstruturaCondicionalExercicioProposto04/src/application/Main.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.util.Scanner;
+
+/**
+ * AUTOR:GABRIEL GAMELEIRA DOS SANTOS
+ * DATA:29/01/2019
+ * HORA:19:22
+ * FINALIDADE DO PROGRAMA:LER O INICIO E FIM DE UM JOGO E SABER SEU TEMPO DE DURACAO
+ */
+
+public class Main {
+	
+		public static void main(String[]args) {
+			
+			int horaInicial, horaFinal, duracao = 0;
+			
+			int duracaoMin;
+			
+			int duracaoMax;
+			
+			Scanner dados;
+			
+			dados = new Scanner(System.in);
+			
+			duracaoMin = 1;
+			
+			duracaoMax = 24;
+			
+			System.out.println("Informar a hora inicial do jogo:");
+			
+			horaInicial = dados.nextInt();
+			
+			System.out.println("Informar a hora final do jogo:");
+			
+			horaFinal = dados.nextInt();
+			
+			if(horaFinal > horaInicial) {
+				
+				duracao = horaFinal - horaInicial;
+				
+			} else {
+				
+				duracao =  24 - horaInicial; 
+				
+				duracao += horaFinal; 
+				
+			}
+			
+			if(duracao > duracaoMin && duracao <= duracaoMax) {
+				
+				System.out.println("O jogo durou:" + duracao +  " horas.");
+				
+			} else {
+				
+				System.out.println("Não é possivel calcular pois os momentos não respeitaram o min e max de duração de um jogo.");
+				
+			}
+			
+			
+			
+		}
+
+}
